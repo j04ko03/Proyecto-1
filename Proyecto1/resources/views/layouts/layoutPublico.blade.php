ed38b3d45f105e363f14c2fa26b0b4654668d4dd
josepguiudev <josep.guiu.dev@gmail.com>
2025-11-13 04:03:30 +0100

    login maquetacion

--- Proyecto1/resources/views/layouts/layoutPublico.blade.php
+++ Proyecto1/resources/views/layouts/layoutPublico.blade.php
@@ -1,25 +1,34 @@
-@push('styles')
-    <link rel="stylesheet" href="">
-@endpush
-
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title class="pestaña">Juegos</title>
+    <title>Juegos</title>
+
+    <!-- Tailwind CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+
+    <!-- CSS base -->
+    <link rel="stylesheet" href="{{ asset('assets/css/layoutPublicoPrivado.css') }}">
+
+    <!-- CSS de vistas hijas -->
     @stack('styles')
 </head>
 
-<body class="bg-gray-50">
+<body class="background">
 
     <!-- Contenido principal -->
     <main class="main-content">
+        <div class="contenedorTitulo">
+            <h1>Bienvenid@ Gamer</h1>
+        </div>
         @yield('content')
     </main>
 
     @stack('scripts')
 </body>
-</html>
+</html> 
---
+++ Proyecto1/resources/views/login/login.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.layoutPublico')
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+@endpush
+
+@section('content')
+    <div class="contenedorPrincipal">
+        <div class="contenedorLogin">
+            <div class="contenedorConForma">
+                <form id="">
+                    <label for="email">Email/Usuario</label>
+                    <input type="text" id="email" required autocomplete="username"/>
+
+                    <label for="password" style="margin-top: 5%">Contraseña</label>
+                    <input type="password" id="password" required autocomplete="current-password"/>
+
+                    <div class="links" style="margin-top: 5%">
+                        <a href="#">Crear cuenta</a>
+                        <button type="submit" class="btn-login">Iniciar sesión</button>
+                    </div>
+                </form>
+
+                <img src="ruta-del-avion.png" alt="Avion" class="avion" />
+            </div>
+        </div>
+    </div>
+@endsection
