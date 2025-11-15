ac42360b502670f555eb6b41db7d6eba4ca65bed
josepguiudev <josep.guiu.dev@gmail.com>
2025-11-15 22:36:58 +0100

    NavBar

---
+++ Proyecto1/resources/views/barraNavegacion.blade.php
@@ -0,0 +1,24 @@
+<div class="retro-navbar" style="border: 1px solid purple; height: 10%; width: 100%;">
+    
+    <!-- NOMBRE DEL USUARIO -->
+    <div class="nombre">
+        <span class="arroba">@</span> {{Auth::user()->nickName}}
+    </div>
+
+    <!-- BUSCADOR -->
+    <div class="buscador">
+        <input type="text" id="searchInput" placeholder="Buscar juego...">
+    </div>
+
+    <!-- ENGRANAJE -->
+    <div class="opciones">
+        <img src="{{ asset('img/iconoSettings.png') }}" alt="Opciones" class="iconoEngranaje">
+
+        <div class="dropdown">
+            <a href="#">Configuración</a>
+            <a href="#">Perfil</a>
+            <a href="#">Cerrar sesión</a>
+        </div>
+    </div>
+
+</div>
--- Proyecto1/resources/views/layouts/layoutPrivado.blade.php
+++ Proyecto1/resources/views/layouts/layoutPrivado.blade.php
@@ -6,12 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title class="pestaña">JUEGOS</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- CSS base -->
+    <link rel="stylesheet" href="{{ asset('assets/css/layoutPublicoPrivado.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/barraNavegacion.css') }}">
+
+    <!-- CSS de vistas hijas -->
     @stack('styles')
 </head>
 
-<body class="bg-gray-50">
+<body class="background">
     <!-- Navbar -->
-    @include('barraNavegacion')
+    @include('barraNavegacion', ['usuario' => Auth::user()->nickName])
     
     <!-- Contenido principal -->
     <main class="main-content">
