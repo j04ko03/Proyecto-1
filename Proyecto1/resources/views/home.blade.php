5e8b1da4ee83fdf1612a211738bc8adfa38dd8a6
josepguiudev <josep.guiu.dev@gmail.com>
2025-11-15 23:11:16 +0100

    Pequeña pantalla de home

--- Proyecto1/resources/views/home.blade.php
+++ Proyecto1/resources/views/home.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.layoutPrivado')
 @push('styles')
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 @endpush
 
 @section('content')
 
+<div>
+    <div class="contenedorPrincipalHome" style="border: 1px solid red; width: 100%; height: 600px; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+        <a style="color: white" href="#">Aqui va la pantalla de los juegos</a>
+        <div id="pantallaJugable" style="border: 2px solid white; width: 60%; height: 60%;">
 
+        </div>
+    </div>
+    <div class="contenedorPrincipalHome" style="border: 1px solid red; width: 100%; height: 240px; display: flex; flex-direction: column; justify-content: center; align-items: center;">
 
-<script src="{{ url('/js/X.js') }}"></script>
+    </div>
+</div>
+
 @endsection
