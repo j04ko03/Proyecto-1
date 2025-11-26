ae3883d4d719ee492000728c3bf05a6805aed84f
Jeancarlos Félix Guevara <jfelixg2324@politecnics.barcelona>
2025-11-26 04:52:54 +0100

    Cambiado blade, css y js
    Creado la logica de obstaculos, personaje y perdida de vida

--- Proyecto1/public/js/CapiMates/CapiMates.js
+++ Proyecto1/public/js/CapiMates/CapiMates.js
@@ -1,25 +1,130 @@
 window.iniciarCapiMates = function () {
     console.log("🚀 CapiMates Jugable iniciado");
 
+    let juegoActivo = false;
+    let rocaInterval, troncoInterval;
+    let animacionId;
+    let obstaculosArray = [];
+
+
+    /* ------------- UI CAPIMATES izq ------------- */
+    let vidas = 3;
+
+
+    /* ------------- UI CAPIMATES der ------------- */
+    let puntos = 0;
+
     const msg = document.getElementById('mensaje');
     const startBtn = document.getElementById('start-btn');
+    const juegoCapiMates = document.getElementById('juegoCapiMates');
+    const personaje = document.getElementById('personaje');
+    const enemigoContainer = document.getElementById('enemigos');
 
     startBtn.addEventListener('click', () => {
         console.log("Iniciar juego pulsado, cerrando Mensaje....");
         ocultarMensaje();
-        iniciarAstro();
+        iniciarCapiMates();
     });
 
     function mostrarMensaje(title, body) {
         msg.style.display = 'block';
         document.getElementById('msg-title').textContent = title;
         document.getElementById('msg-body').innerHTML = body;
-        gameActive = false;
+        juegoActivo = false;
+
+        detenerObstaculos();
+        juegoCapiMates.classList.add("paused");
     }
 
     function ocultarMensaje() {
         msg.style.display = 'none';
-        gameActive = true;
+        juegoActivo = true;
+
+        juegoCapiMates.classList.remove("paused");
+    }
+
+    // Logica de obstaculos
+
+    function generadorObstaculos() {
+        if (!juegoActivo) return;
+
+            rocaInterval = setInterval(() => {
+                let roca = document.createElement("div");
+                roca.classList.add("obstaculo");
+
+                enemigoContainer.appendChild(roca);
+                obstaculosArray.push(roca);
+
+            }, 3000);
+
+            troncoInterval = setInterval(() => {
+                let tronco = document.createElement("div");
+                tronco.classList.add("obstaculo1");
+
+                enemigoContainer.appendChild(tronco);
+                obstaculosArray.push(tronco);
+
+            }, 8000);
+    }
+
+    function detenerObstaculos(){
+        clearInterval(rocaInterval);
+        clearInterval(troncoInterval);
+
+        for (let obstaculo of obstaculosArray) {
+            obstaculo.classList.add("paused");
+        }
+    }
+
+    function detectarColisiones() {
+        let pjRect = personaje.getBoundingClientRect();
+
+        obstaculosArray.forEach((obs, i) => {
+            let oRect = obs.getBoundingClientRect();
+
+            if (!(pjRect.right < oRect.left ||
+                pjRect.left > oRect.right ||
+                pjRect.bottom < oRect.top ||
+                pjRect.top > oRect.bottom)) {
+
+                perderVida();
+
+                obs.remove();
+                obstaculosArray.splice(i, 1);
+            }
+        });
+    }
+
+    function perderVida() {
+        vidas--;
+        document.getElementById("vidas").textContent = "Vidas: " + vidas;
+
+        if (vidas <= 0) {
+            juegoActivo = false;
+            detenerObstaculos();
+            mostrarMensaje("Juego Terminado", "Has perdido todas tus vidas. Pulsa JUGAR para reiniciar.");
+        }
+    }
+
+    //Iniciar el Juego, Ver para futuro implementar mas niveles
+    function iniciarCapiMates() {
+        juegoActivo = true;
+        puntos = 0;
+        vidas = 3;
+
+        generadorObstaculos();
+        loop();
+    }
+
+    function loop() {
+        if (!juegoActivo) {
+            animacionId = null;
+            return;
+        }
+
+        detectarColisiones();
+
+        animacionId = requestAnimationFrame(loop);
     }
 
     /* Iniciar mostrando mensaje inicial */
