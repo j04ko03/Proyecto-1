ea7bf95b5fb70c87414fd03b7145bef52cb9511f
Jeancarlos Félix Guevara <jfelixg2324@politecnics.barcelona>
2025-11-28 14:21:31 +0100

    cambios en el juego

--- Proyecto1/public/js/CapiMates/CapiMates.js
+++ Proyecto1/public/js/CapiMates/CapiMates.js
@@ -10,15 +10,17 @@ window.iniciarCapiMates = function () {
     /* ------------- UI CAPIMATES izq ------------- */
     let vidas = 3;
 
-
     /* ------------- UI CAPIMATES der ------------- */
     let puntos = 0;
+    let ultimoPuntoTiempo = 0;
 
     const msg = document.getElementById('mensaje');
     const startBtn = document.getElementById('start-btn');
     const juegoCapiMates = document.getElementById('juegoCapiMates');
     const personaje = document.getElementById('personaje');
     const enemigoContainer = document.getElementById('enemigos');
+    // const corazones = document.getElementById("corazon");
+    // const vidasPanel = document.getElementById("vidasPanel");
 
     startBtn.addEventListener('click', () => {
         console.log("Iniciar juego pulsado, cerrando Mensaje....");
@@ -48,26 +50,26 @@ window.iniciarCapiMates = function () {
     function generadorObstaculos() {
         if (!juegoActivo) return;
 
-            rocaInterval = setInterval(() => {
-                let roca = document.createElement("div");
-                roca.classList.add("obstaculo");
+        rocaInterval = setInterval(() => {
+            let roca = document.createElement("div");
+            roca.classList.add("obstaculo");
 
-                enemigoContainer.appendChild(roca);
-                obstaculosArray.push(roca);
+            enemigoContainer.appendChild(roca);
+            obstaculosArray.push(roca);
 
-            }, 3000);
+        }, 3000);
 
-            troncoInterval = setInterval(() => {
-                let tronco = document.createElement("div");
-                tronco.classList.add("obstaculo1");
+        troncoInterval = setInterval(() => {
+            let tronco = document.createElement("div");
+            tronco.classList.add("obstaculo1");
 
-                enemigoContainer.appendChild(tronco);
-                obstaculosArray.push(tronco);
+            enemigoContainer.appendChild(tronco);
+            obstaculosArray.push(tronco);
 
-            }, 8000);
+        }, 8000);
     }
 
-    function detenerObstaculos(){
+    function detenerObstaculos() {
         clearInterval(rocaInterval);
         clearInterval(troncoInterval);
 
@@ -95,9 +97,18 @@ window.iniciarCapiMates = function () {
         });
     }
 
+
+    // Logica de vidas y puntos
+
     function perderVida() {
+
+        const corazones = document.getElementById(`corazon${vidas}`);
+
+        if (corazones) {
+            corazones.remove();
+        }
+
         vidas--;
-        document.getElementById("vidas").textContent = "Vidas: " + vidas;
 
         if (vidas <= 0) {
             juegoActivo = false;
@@ -106,23 +117,49 @@ window.iniciarCapiMates = function () {
         }
     }
 
+    function hideQuiz() {
+        document.getElementById("quizPopup").classList.add("hidden");
+      }
+
+    function ganarPuntos(cantidad = 1) {
+        puntos += cantidad;
+        document.getElementById("puntaje").textContent = puntos;
+    }
+
+    // function validarRespuesta() {
+    //     if (respuestaCorrecta) {
+    //         ganarPuntos(2);
+    //     }
+    // }
+
     //Iniciar el Juego, Ver para futuro implementar mas niveles
+
     function iniciarCapiMates() {
         juegoActivo = true;
         puntos = 0;
         vidas = 3;
 
+
+
         generadorObstaculos();
         loop();
     }
 
-    function loop() {
+
+    // Cosas que se tienen que repetir en el juego
+
+    function loop(timestamp) {
         if (!juegoActivo) {
             animacionId = null;
             return;
         }
 
         detectarColisiones();
+
+        if (timestamp - ultimoPuntoTiempo > 1000) {
+            ganarPuntos(1);
+            ultimoPuntoTiempo = timestamp;
+        }
 
         animacionId = requestAnimationFrame(loop);
     }
--- Proyecto1/resources/views/UIGames/UICapiMatesGame.blade.php
+++ Proyecto1/resources/views/UIGames/UICapiMatesGame.blade.php
@@ -1,14 +1,17 @@
 <div id="juegoCapiMates" class="juegoCapiMates">
+
+    {{-- Panel del juego --}}
+
     <div id="ui" class="ui uiCapi">
         <div class="ui-left vidas-container">
             <div class="vidas-inner">
                 <div class="vidas-label">
-                    <span id="vidas"> Vidas: </span>
+                    <span id="contadorVidas"> Vidas</span>
                 </div>
-                <div class="vidas-icons">
-                    <img src="/Proyecto-1/Proyecto1/Astro/corazon.png" alt="Imagen de vida" class="vida-img">
-                    <img src="/Proyecto-1/Proyecto1/Astro/corazon.png" alt="Imagen de vida" class="vida-img">
-                    <img src="/Proyecto-1/Proyecto1/Astro/corazon.png" alt="Imagen de vida" class="vida-img">
+                <div class="vidas-icons" id="vidasPanel">
+                    <img id="corazon3" src="/Proyecto-1/Proyecto1/Astro/corazon.png" alt="Imagen de vida" class="vida-img">
+                    <img id="corazon2" src="/Proyecto-1/Proyecto1/Astro/corazon.png" alt="Imagen de vida" class="vida-img">
+                    <img id="corazon1" src="/Proyecto-1/Proyecto1/Astro/corazon.png" alt="Imagen de vida" class="vida-img">
                 </div>
             </div>
         </div>
@@ -18,7 +21,7 @@
         </div>
     </div>
 
-    <!-- Story de capi -->
+    <!-- Mensaje del principio -->
     <div id="mensaje" class="mensaje">
         <h2 id="msg-title" class="msg-title"></h2>
         <div id="msg-body" class="msg-body"></div>
@@ -27,8 +30,27 @@
         </div>
     </div>
 
-    <div class="ground"></div>
+    {{-- Objetos del juego suelo y personaje  --}}
     <div id="personaje" class="personaje"></div>
     <div id="enemigos"></div>
     <div class="piso"></div>
+
+    {{-- Contenedor Preguntas --}}
+    <div id="quizPopup" class="quiz-popup">     {{-- Volver a poner hidden --}}
+
+        <div class="quiz-header">
+          <span class="quiz-title">Preguntas del bosque</span>
+          <span class="quiz-close" onclick="hideQuiz()">✖</span>
+        </div>
+
+        <div class="quiz-body">
+
+            <p id="quizQuestion">¿Cuántas manzanas ves?</p>
+
+          <input id="quizAnswer" type="text" placeholder="Escribe tu respuesta"/>
+
+          <button onclick="submitQuiz()">Responder</button>
+
+        </div>
+    </div>
 </div>
