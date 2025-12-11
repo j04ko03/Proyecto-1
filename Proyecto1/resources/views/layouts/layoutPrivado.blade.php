d8ece5e0c73f990c0cc73719ff76cab39362d067
Jeancarlos Félix Guevara <jfelixg2324@politecnics.barcelona>
2025-12-11 05:50:16 +0100

    cambios en barra navegacion

--- Proyecto1/resources/views/layouts/layoutPrivado.blade.php
+++ Proyecto1/resources/views/layouts/layoutPrivado.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>JUEGOS</title>
-    
+
     <script src="https://cdn.tailwindcss.com"></script>
-    
+
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400;700&display=swap" rel="stylesheet">
 
@@ -31,12 +32,12 @@
     </main>
 
     @include('components.desplegable')
-    
+
     <!-- SCRIPTS GLOBALES (se cargan siempre) -->
     <script src="{{ asset('js/usuarios.js') }}"></script>
     <script src="{{ asset('js/1BotonesJuegos.js') }}"></script>
     <script src="{{ asset('js/consolaResizeManager.js') }}"></script>
-    
+
     <script>
         window.rutaScripts = {
             redimensionador: "{{ asset('js/RedimensionCava.js') }}",
@@ -46,4 +47,5 @@
     <!-- SCRIPTS DE VISTAS HIJAS (se agregan dinámicamente) -->
     @stack('scripts')
 </body>
+
 </html>
